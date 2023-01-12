c5328316cab4db347c656b3b27e7c2011267586d
arif-islam <cse.arif99@gmail.com>
2023-01-12 18:15:14 +0600

    dashboard

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index'),
-    path('staff/', views.staff, name='staff'),
-    path('products/', views.products, name='products'),
-    path('orders/', views.orders, name='orders'),
-    path('profile/', views.profile, name='profile'),
+    # path('', views.index, name='index'),
+    # path('staff/', views.staff, name='staff'),
+    # path('products/', views.products, name='products'),
+    # path('orders/', views.orders, name='orders'),
+    # path('profile/', views.profile, name='profile'),
     ]
--- inventory/views.py
+++ inventory/views.py
@@ -5,18 +5,18 @@ from django.template import loader
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Allahu Akbar")
-def index(request):
-        template=loader.get_template('master.html')
-        return HttpResponse(template.render())
-def staff(request):
-        template=loader.get_template('master.html')
-        return HttpResponse(template.render())
-def products(request):
-        template=loader.get_template('master.html')
-        return HttpResponse(template.render())
-def orders(request):
-        template=loader.get_template('master.html')
-        return HttpResponse(template.render())
-def profile(request):
-        template=loader.get_template('master.html')
-        return HttpResponse(template.render())
+# def index(request):
+#         template=loader.get_template('master.html')
+#         return HttpResponse(template.render())
+# def staff(request):
+#         template=loader.get_template('master.html')
+#         return HttpResponse(template.render())
+# def products(request):
+#         template=loader.get_template('master.html')
+#         return HttpResponse(template.render())
+# def orders(request):
+#         template=loader.get_template('master.html')
+#         return HttpResponse(template.render())
+# def profile(request):
+#         template=loader.get_template('master.html')
+#         return HttpResponse(template.render())
